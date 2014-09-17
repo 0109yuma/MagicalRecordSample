bb46ea3ae842ad035d64c8674f476c5f48c7f61f
Yuma Oda <wyc00nny@gmail.com>
2014-09-17 18:12:52 +0900

    setup方法の変更

--- MagicalRecordTest/AppDelegate.m
+++ MagicalRecordTest/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreNamed:@"MagicalRecordTest.sql"];
+    [MagicalRecord setupAutoMigratingCoreDataStack];
     
     return YES;
 }
