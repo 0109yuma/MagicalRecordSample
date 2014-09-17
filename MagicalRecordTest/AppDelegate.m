e99ae0cc1d8717950ae826dd16ced1f46b6c25ae
Yuma Oda <wyc00nny@gmail.com>
2014-09-17 16:04:39 +0900

    非同期処理追加

--- MagicalRecordTest/AppDelegate.m
+++ MagicalRecordTest/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    [MagicalRecord setupCoreDataStackWithStoreNamed:@"MagicalRecordTest.sql"];
+    [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreNamed:@"MagicalRecordTest.sql"];
     
     return YES;
 }
--- MagicalRecordTest/ViewController.m
+++ MagicalRecordTest/ViewController.m
@@ -10,6 +10,7 @@
 #import "CoreData+MagicalRecord.h"
 #import "Bike.h"
 
+
 @interface ViewController ()
 
 @end
@@ -20,12 +21,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    [self saveBySync];
+    [self saveByAsync];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)saveBySync
+{
+    // 同期
     Bike *bike = [Bike MR_createEntity];
     bike.name = @"kana_bike";
     bike.comment = @"bad";
     bike.tag = @"america";
     [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-    
+
+    NSLog(@"count::%d", [[Bike MR_numberOfEntities] intValue]);
     NSArray *bikeArray = [Bike MR_findAll];
     [bikeArray enumerateObjectsUsingBlock:^(Bike *obj, NSUInteger idx, BOOL *stop) {
         NSLog(@"--------------------------");
@@ -35,9 +50,29 @@
     }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)saveByAsync
+{
+    // 非同期
+    [MagicalRecord saveWithBlock:^(NSManagedObjectContext *localContext) {
+        Bike *localBike = [Bike MR_createInContext:localContext];
+        localBike.name = @"tanaka";
+        localBike.comment = @"wonderful";
+        localBike.tag = @"korea";
+        
+    } completion:^(BOOL success, NSError *error) {
+        if (!success) {
+            
+        } else {
+            NSLog(@"count::%d", [[Bike MR_numberOfEntities] intValue]);
+            NSArray *bikeArray = [Bike MR_findAll];
+            [bikeArray enumerateObjectsUsingBlock:^(Bike *obj, NSUInteger idx, BOOL *stop) {
+                NSLog(@"--------------------------");
+                NSLog(@"name::%@", obj.name);
+                NSLog(@"comment::%@", obj.comment);
+                NSLog(@"tag::%@", obj.tag);
+            }];
+        }
+    }];
 }
 
 @end
